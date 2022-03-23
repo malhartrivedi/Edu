08d59b8bf73c33db2debb9701f918bdb7391e6b4
JayP-Felixphase <jay.patel@silverxis.com>
2022-03-23 11:00:49 +0000

    - git ignore updated - added textstyle class

---
+++ lib/widgets/my_textstyle.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:admin/utils/app_fonts.dart';
+
+final h1TextStyle = TextStyle(
+  fontSize: 24.sp,
+  fontWeight: fwBold,
+);
